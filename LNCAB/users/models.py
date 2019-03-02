b0bc6c042e4c011a77ced14602ebb07df1537411
EduardoCuestaC <A00570171@itesm.mx>
2019-03-01 23:40:30 -0600

    ide updates

--- LNCAB/users/models.py
+++ LNCAB/users/models.py
@@ -6,6 +6,6 @@ from django.db import models
 class Coach(models.Model):
     name = models.CharField(max_length=200)
     last_names = models.CharField(max_length=200)
-    telephone = models.CharField(max_length=100)
+    telephone = git models.CharField(max_length=100)
     start_date = models.DateTimeField("start date")
     end_date = models.DateTimeField("start date")
