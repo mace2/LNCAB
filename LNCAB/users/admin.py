b614a0631745ac937f6a6ca55ce45d157ee3b68b
Andres Quiroz <andresq1595@gmail.com>
2019-03-07 11:52:16 -0600

    CreateTeam

--- LNCAB/users/admin.py
+++ LNCAB/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Coach,Team
+
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Coach,UserAdmin)
+
+class TeamAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Team,TeamAdmin)
