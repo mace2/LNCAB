9b6de240e5a949d14c30b47b5f246264db7984b2
EduardoCuestaC <A00570171@itesm.mx>
2019-03-08 17:10:07 -0600

    add tournament admin

--- LNCAB/tournaments/admin.py
+++ LNCAB/tournaments/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Game, Tournament
 # Register your models here.
+
+
+class GameAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Game, GameAdmin)
+
+
+class TournamentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Tournament, TournamentAdmin)
+
+
