9af8b28b0a2d443a9f81f33f41da038d7dcfea68
Andres Quiroz <andresq1595@gmail.com>
2019-03-08 13:00:42 -0600

    Base.html jalando

--- LNCAB/teams/views.py
+++ LNCAB/teams/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Team
 
-# Create your views here.
+
+def index(request):
+    template = loader.get_template('teams/base.html')
+    teams = Team.objects.all()
+    context={"teams":teams,
+    }
+
+    return HttpResponse(template.render(context,request))
