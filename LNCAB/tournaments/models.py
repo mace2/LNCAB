4279573b9da1c7f38af9daea30b7f3bf82885cf0
Andres Quiroz <andresq1595@gmail.com>
2019-03-11 16:56:13 -0600

    Crear JornadA

--- LNCAB/tournaments/models.py
+++ LNCAB/tournaments/models.py
@@ -11,14 +11,35 @@ class Tournament(models.Model):
     def __str__(self):
         return self.name
 
+class Jornada(models.Model):
+    torneo=models.ForeignKey(Tournament,on_delete=models.CASCADE)
+    opciones=(
+        ('IZ','InterZona'),
+        ('Z','Zona'),
+    )
+    start_date=models.DateTimeField("Start Date")
+    end_date = models.DateTimeField("End Date")
+    tipo=models.CharField(max_length=2,choices=opciones)
+
+
+    def __str__(self):
+
+        return self.torneo.name+" Jornada "+str(self.pk)+" ("+self.tipo +")"
+
+
 
 class Game(models.Model):
     date = models.DateTimeField("date")
-    tournament_id = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
     teamLocal=models.ForeignKey('teams.Team', on_delete=models.CASCADE,related_name='LocalTeam')
     teamVisitante = models.ForeignKey('teams.Team', on_delete=models.CASCADE, related_name='VisitanteTeam')
+    jornada=models.ForeignKey(Jornada,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return "Juego "+str(self.pk)+ " de "+str(self.jornada)
+
+
+
+
 
 
