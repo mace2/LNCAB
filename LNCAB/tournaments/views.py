8494404ecf3c665d3971b18399ae0d31421fac20
EduardoCuestaC <A00570171@itesm.mx>
2019-03-15 16:15:43 -0600

    almost finish view game

--- LNCAB/tournaments/views.py
+++ LNCAB/tournaments/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 # Create your views here.
 
 from django.views import generic
-from .models import Game, Tournament
+from .models import Game, Tournament, Day
 
 
 class GamesView(generic.ListView):
     template_name = 'tournaments/index.html'
 
     def get_queryset(self):
-        return Game.objects.all()
+        try:
+            self.day = Day.objects.get(number=self.kwargs['day'])
+        except Day.DoesNotExist:
+            raise Http404()
+        return Game.objects.filter(day=self.day)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["day"] = self.day
+        return context
 
