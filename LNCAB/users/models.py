cf81f12d6fb7c452e6162cdd5ddaa3efac8296c6
ManuelGarcia <A01207405@itesm.mx>
2019-03-15 17:16:07 -0600

    merge conflicts

--- LNCAB/users/models.py
+++ LNCAB/users/models.py
@@ -13,3 +13,19 @@ class Coach(models.Model):
 
     def __str__(self):
         return self.name+" "+self.last_names
+<<<<<<< .merge_file_a02916
+=======
+
+class Team(models.Model):
+    name= models.CharField("Nombre de Equipo",max_length=50)
+    nombre_Coach = models.OneToOneField(Coach, on_delete=models.CASCADE)
+    city=models.CharField("Ciudad",default="Temp",max_length=50)
+    cede = models.CharField("Cede",max_length=200)
+
+
+    def __str__(self):
+        return self.name
+
+class Scorekeeper (models.Model):
+  nombre_Scorekeeper = models.CharField("Nombre del Anotador",max_length=50)
+>>>>>>> .merge_file_a02984
