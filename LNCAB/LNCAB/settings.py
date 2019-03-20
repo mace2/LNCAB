3ffe1c518207226726561fae2d02066b4f15cd70
A01700043 <A01700043@itesm.mx>
2019-03-20 15:58:33 -0600

    modificacion set
    settings

--- LNCAB/LNCAB/settings.py
+++ LNCAB/LNCAB/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'LNCAB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'lncab',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
