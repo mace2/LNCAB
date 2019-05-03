b50913b45d3c0f9c6d07c053990016af8b7c88c0
Andres Quiroz <andresq1595@gmail.com>
2019-05-02 20:28:18 -0500

    Css

--- LNCAB/tournaments/views.py
+++ LNCAB/tournaments/views.py
@@ -295,7 +295,7 @@ class TeamDetailView(generic.DetailView):
 
 
 class StatisticsViewState(generic.TemplateView):
-    template_name = "tournaments/stats.html"
+    template_name = "tournaments/statsState.html"
 
     class StandingsEntry:
         def __init__(self, place, team, value):
