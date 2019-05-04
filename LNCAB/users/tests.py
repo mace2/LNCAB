5cfb3f75f149ea1f2ea31865d16b30bed2005ea6
Andres Quiroz <andresq1595@gmail.com>
2019-05-04 00:43:14 -0500

    1st Test(User_Form)

--- LNCAB/users/tests.py
+++ LNCAB/users/tests.py
@@ -5,6 +5,7 @@ from teams.models import Team, State
 from users.models import Coach, Player
 from tournaments.models import Tournament, Day, Game, Venue, Point, Foul, Win
 from django.contrib.auth.models import User
+from .forms import UserForm
 from .forms import PlayerForm
 
 
@@ -28,30 +29,80 @@ from .forms import PlayerForm
 #         pass
 
 
-class PlayerFormTests(TestCase):
+#class PlayerFormTests(TestCase):
+ #   def test_create(self):
+  #      u = User.objects.create_user(
+   #         username="uname",
+    #        email="uemail",
+     #       password="upass",
+      #      first_name="ufirst",
+       #     last_name="ulast"
+        #)
+        #self.client.force_login(u)
+        #s = State(1, "Prueba", "PRB")
+        #s.save()
+        #t1 = Team(1, state=s, address="addprueba1", name="teamprueba1")
+        #t1.save()
+        #t1.generate_code()
+        #data = {
+         #   "code": t1.code,
+          #  "date_of_birth": "01/15/2001",
+           # "telephone": "1234567890",
+            #"sex": "F"
+        #}
+        #self.client.post("/users/playerform.html/", data)
+        #p = Player.objects.get(id=1)
+        #self.assertEqual(str(p), u.first_name + " " + u.last_name)
+
+
+
+#Andres Quiroz Test 1
+class UserFormTests(TestCase):
     def test_create(self):
-        u = User.objects.create_user(
-            username="uname",
-            email="uemail",
-            password="upass",
-            first_name="ufirst",
-            last_name="ulast"
-        )
-        self.client.force_login(u)
-        s = State(1, "Prueba", "PRB")
-        s.save()
-        t1 = Team(1, state=s, address="addprueba1", name="teamprueba1")
-        t1.save()
-        t1.generate_code()
-        data = {
-            "code": t1.code,
-            "date_of_birth": "01/15/2001",
-            "telephone": "1234567890",
-            "sex": "F"
-        }
-        self.client.post("/users/playerform.html/", data)
-        p = Player.objects.get(id=1)
-        self.assertEqual(str(p), u.first_name + " " + u.last_name)
+
+       u=User.objects.create_user(
+           username="uname",
+           email="uemail",
+           password="upass",
+           first_name="ufirst",
+           last_name="ulast"
+       )
+
+
+       us = User.objects.get(id=1)
+       self.assertEqual(str(us), u.username)
+
+    def test_form_valid_password(self):
+         data = UserForm({
+            "username" :"usname",
+            "first_name" : "uname",
+            "last_name" : "lname",
+            "email" : "umail@mail.itesm.mx",
+            "password": "upass",
+            "password2":"upass",
+         })
+
+         self.assertTrue(data.is_valid())
+
+    def test_form_not_valid_password(self):
+         data = UserForm({
+            "username" :"usname",
+            "first_name" : "uname",
+            "last_name" : "lname",
+            "email" : "umail@mail.itesm.mx",
+            "password": "upass",
+            "password2":"upass2",
+         })
+
+         self.assertFalse(data.is_valid())
+
+#AndresQuirozTest2
+
+
+
+
+
+
 
 
 
