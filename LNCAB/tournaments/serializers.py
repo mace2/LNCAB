a0575983f10566435ac068b839a9102b58488faa
EduardoCuestaC <A00570171@itesm.mx>
2019-05-09 15:02:59 -0500

    get all and by id api

--- LNCAB/tournaments/serializers.py
+++ LNCAB/tournaments/serializers.py
@@ -3,11 +3,15 @@ from tournaments.models import Game
 
 
 class GameSerializer(serializers.ModelSerializer):
-    team_local = serializers.StringRelatedField(read_only=True)
-    team_visitor = serializers.StringRelatedField(read_only=True)
+    team_local = serializers.SlugRelatedField(read_only=True, slug_field="name")
+    team_visitor = serializers.SlugRelatedField(read_only=True, slug_field="name")
 
     class Meta:
         model = Game
-        fields = ('id','team_local','team_visitor','local_points','visitor_points',
+        fields = ('id', 'team_local', 'team_visitor','local_points','visitor_points',
                   'local_q1','local_q2','local_q3','local_q4','visitor_q1','visitor_q2',
                   'visitor_q3','visitor_q4','local_points','visitor_points')
+
+    def update(self, instance, validated_data):
+        print(validated_data)
+        return instance
